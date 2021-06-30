71b0642c457b1cd07f65eacbdadc9cdba07052af
xiaoyuren <15773003996@163.com>
2021-07-01 00:10:47 +0800

    主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,59 +2,181 @@
 const app = getApp()
 Page({
   data: {
-    newGoods: [],
-    hotGoods: [],
-    topics: [],
-    brands: [],
-    floorGoods: [],
+    tabs: [],
+    activeTab: 0,
     banner: [
       {
         id:'1',
         link:'',
-        image_url:''
+        image_url:'/static/images/banner1.jpg'
+      },
+      {
+        id:'2',
+        link:'',
+        image_url:'/static/images/banner2.jpg'
+      },
+      {
+        id:'3',
+        link:'',
+        image_url:'/static/images/banner3.jpg'
       }
     ],
-    channel: []
+    brand:[
+      {
+        id:'1',
+        desc:'和正成立于2019年，是一家以企业财税服务、顾问咨询为重点，集财税、商标、专利、网络、传媒为一体的现代化企业',
+        image_url:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/18/192020051807060482645.png'
+      },
+      {
+        id:'2',
+        desc:'一年来已经成功服务多家企业，年服务次数超100次，且每季客户数据呈逐季递增，成为湖南最具潜质的新兴企业服务公司之一',
+        image_url:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/18/192020051807573668590.png'
+      },
+      {
+        id:'3',
+        desc:'和正企业服务从创立之初至今，团队迅速发展，目前已有五大事业部，已有共计人数100+的专业服务团队。',
+        image_url:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/18/202020051808030758290.png'
+      }
+    ]
   },
   onShareAppMessage: function () {
     return {
-      title: 'NideShop',
-      desc: '仿网易严选微信小程序商城',
+      title: '和正企业',
+      desc: '和正企业',
       path: '/pages/index/index'
     }
   },
-
-  getIndexData: function () {
-    let that = this;
-    console.log(app)
-    app.$api.homeApi.getHomeInfo().then(function (res) {
-      console.log(res)
-      if (res.errno === 0) {
-        that.setData({
-          newGoods: res.data.newGoodsList,
-          hotGoods: res.data.hotGoodsList,
-          topics: res.data.topicList,
-          brand: res.data.brandList,
-          floorGoods: res.data.categoryList,
-          banner: res.data.banner,
-          channel: res.data.channel
-        });
-      }
-    });
-  },
   onLoad: function (options) {
-    this.getIndexData();
+    const tabs = [
+      {
+        title: '公司注册',
+        tabContent:[
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312061734288.gif',
+            title:'内资公司注册',
+            desc:'专业领域，一站服务'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312102341319.gif',
+            title:'个体户注册',
+            desc:'轻松注册，灵活经营'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312071176539.gif',
+            title:'前海公司注册',
+            desc:'抢占先机，开发市场'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312073672931.gif',
+            title:'香港公司注册',
+            desc:'拓展海外市场'
+          }
+        ]
+      },
+      {
+        title: '财税服务',
+        tabContent:[
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312140819845.gif',
+            title:'代理记账',
+            desc:'全程服务，可视化代理'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312143237122.gif',
+            title:'一般纳税人',
+            desc:'开具税票，减少风险'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312151525500.gif',
+            title:'专项审计',
+            desc:'提供资料，免费开户'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/21/232020052111034436854.gif',
+            title:'进出口业务',
+            desc:'放眼世界，触摸世界'
+          }
+        ]
+      },
+      {
+        title: '工商服务',
+        tabContent:[
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312081432008.gif',
+            title:'公司注销',
+            desc:'全程代办，省时省力'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312085682062.gif',
+            title:'股权变更',
+            desc:'提供资料，全程代办'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312094953332.gif',
+            title:'地址变更',
+            desc:'3-5天完成变更'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312111226641.gif',
+            title:'经营范围变更',
+            desc:'扩大经营，开拓业务'
+          }
+        ]
+      },
+      {
+        title: 'IT/软件',
+        tabContent:[
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312061734288.gif',
+            title:'网站建设',
+            desc:'专业领域，一站服务'
+          },
+        ]
+      },
+      {
+        title: '知识产权',
+        tabContent:[
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312120122520.gif',
+            title:'专利申请',
+            desc:'独占市场，技术保护'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312124693978.gif',
+            title:'商标设计',
+            desc:'标志设计，提升形象'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312132419974.gif',
+            title:'商标注册',
+            desc:'建立品牌，抢点市场'
+          },
+          {
+            img:'http://www.hzqy168.cn/uploadFiles/uploadImgs/2020/05/13/002020051312134136613.gif',
+            title:'商标延展',
+            desc:'延续品牌，加强形象'
+          }
+        ]
+      },
+    ]
+    this.setData({ tabs })
   },
-  onReady: function () {
-    // 页面渲染完成
+  onTabClick(e) {
+    const index = e.detail.index
+    this.setData({ 
+      activeTab: index 
+    })
   },
-  onShow: function () {
-    // 页面显示
+
+  onChange(e) {
+    const index = e.detail.index
+    this.setData({ 
+      activeTab: index 
+    })
   },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
+  handleClick(e) {
+    wx.navigateTo({
+      url: './webview',
+    })
+  }
 })
---
+++ pages/search/search.js
@@ -0,0 +1,142 @@
+var app = getApp()
+Page({
+  data: {
+    keywrod: '',
+    searchStatus: false,
+    goodsList: [],
+    helpKeyword: [],
+    historyKeyword: [],
+    categoryFilter: false,
+    currentSortType: 'default',
+    currentSortOrder: '',
+    filterCategory: [],
+    defaultKeyword: {},
+    hotKeyword: [],
+    page: 1,
+    size: 20,
+    currentSortType: 'id',
+    currentSortOrder: 'desc',
+    categoryId: 0
+  },
+  //事件处理函数
+  closeSearch: function () {
+    wx.navigateBack()
+  },
+  clearKeyword: function () {
+    this.setData({
+      keyword: '',
+      searchStatus: false
+    });
+  },
+  onLoad: function () {
+    this.getSearchKeyword();
+  },
+
+  getSearchKeyword() {
+    let that = this;
+    //获取搜索关键词
+  },
+
+  inputChange: function (e) {
+    this.setData({
+      keyword: e.detail.value,
+      searchStatus: false
+    });
+    this.getHelpKeyword();
+  },
+  getHelpKeyword: function () {
+    let that = this;
+    //搜搜结果
+  },
+  inputFocus: function () {
+    this.setData({
+      searchStatus: false,
+      goodsList: []
+    });
+
+    if (this.data.keyword) {
+      this.getHelpKeyword();
+    }
+  },
+  clearHistory: function () {
+    this.setData({
+      historyKeyword: []
+    })
+    //清除搜索历史
+  },
+  getGoodsList: function () {
+    let that = this;
+    //搜索结果
+  },
+  onKeywordTap: function (event) {
+
+    this.getSearchResult(event.target.dataset.keyword);
+
+  },
+  getSearchResult(keyword) {
+    this.setData({
+      keyword: keyword,
+      page: 1,
+      categoryId: 0,
+      goodsList: []
+    });
+
+    this.getGoodsList();
+  },
+  openSortFilter: function (event) {
+    let currentId = event.currentTarget.id;
+    switch (currentId) {
+      case 'categoryFilter':
+        this.setData({
+          'categoryFilter': !this.data.categoryFilter,
+          'currentSortOrder': 'asc'
+        });
+        break;
+      case 'priceSort':
+        let tmpSortOrder = 'asc';
+        if (this.data.currentSortOrder == 'asc') {
+          tmpSortOrder = 'desc';
+        }
+        this.setData({
+          'currentSortType': 'price',
+          'currentSortOrder': tmpSortOrder,
+          'categoryFilter': false
+        });
+
+        this.getGoodsList();
+        break;
+      default:
+        //综合排序
+        this.setData({
+          'currentSortType': 'default',
+          'currentSortOrder': 'desc',
+          'categoryFilter': false
+        });
+        this.getGoodsList();
+    }
+  },
+  selectCategory: function (event) {
+    let currentIndex = event.target.dataset.categoryIndex;
+    let filterCategory = this.data.filterCategory;
+    let currentCategory = null;
+    for (let key in filterCategory) {
+      if (key == currentIndex) {
+        filterCategory[key].selected = true;
+        currentCategory = filterCategory[key];
+      } else {
+        filterCategory[key].selected = false;
+      }
+    }
+    this.setData({
+      'filterCategory': filterCategory,
+      'categoryFilter': false,
+      categoryId: currentCategory.id,
+      page: 1,
+      goodsList: []
+    });
+    this.getGoodsList();
+  },
+  onKeywordConfirm(event) {
+    this.getSearchResult(event.detail.value);
+  }
+})
